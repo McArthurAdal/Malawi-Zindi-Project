69f6206444e7184aff8bafc67aadcc674d766440
Mark Kyama <mark.kyama.adal@gmail.com>
2024-02-13 20:02:04 +0000

    built skeleton

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-import streamlit as streamlit
+import streamlit as st
+from dotenv import load_dotenv
 
 def main():
-    st.set_page_config(page_title-"IDSR Chat", page_icon=":books:")
+    st.set_page_config(page_title="IDSR Chat", page_icon=":books:")
+    st.header("IntelSurv Chat")
+    st.text_input("Ask a question")
+
+    with st.sidebar:
+        st.subheader("TG for IDSR Booklet")
+        st.file_uploader("Upload booklet here")
+        st.button("Process")
 
 
 
